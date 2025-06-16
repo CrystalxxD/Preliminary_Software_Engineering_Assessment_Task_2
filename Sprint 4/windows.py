485d4ce4012cadff0759210b551ef9874c1ee24b
CrystalxxD <chris.wong948@education.nsw.gov.au>
2025-06-16 17:47:44 +1000

    Create windows.py

---
+++ Sprint 4/windows.py
@@ -0,0 +1,27 @@
+# Screen dimensions
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+MINIMAP_SIZE = 300
+STATS_AREA_WIDTH = 300
+GAME_AREA_WIDTH = SCREEN_WIDTH - STATS_AREA_WIDTH - MINIMAP_SIZE
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GRAY = (100, 100, 100)
+DARK_GRAY = (50, 50, 50)
+LIGHT_GRAY = (200, 200, 200)
+PINK = (255, 105, 180)
+PURPLE = (128, 0, 128)
+
+# Fonts (initialized in main.py)
+font_small = None
+font_medium = None
+font_large = None
+
+# Game data
+room_stories = []
